26e2a814bd34be5d5b060a356c7446de4963452b
idivyamv <101665482+idivyamv@users.noreply.github.com>
2022-07-02 01:40:18 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -57,5 +57,5 @@ app.get('/',function(req,res){
 
 
 app.listen(5000,()=>{
-    console.log("Server Ready on 3000");
+    console.log("Server Ready on 5000");//server ready at port 5000 not 3000
 });
