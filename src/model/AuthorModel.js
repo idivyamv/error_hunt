9dd4267d4699591346ba2eb9616190fa7b50f9cb
idivyamv <101665482+idivyamv@users.noreply.github.com>
2022-07-03 15:55:20 +0530

    Update AuthorModel.js

--- src/model/AuthorModel.js
+++ src/model/AuthorModel.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/Library');
+mongoose.connect('mongodb+srv://error-hunt:divya12A@cluster0.afrxmm1.mongodb.net/?retryWrites=true&w=majority', { useNewUrlParser: true });
 //mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true });
 
 
